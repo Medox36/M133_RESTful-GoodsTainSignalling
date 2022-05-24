ee36ec23f285026188d36389246410104f612ff0
Medox36 <giuntinil@bzz.ch>
2022-05-24 09:32:22 +0200

    optimized imports

--- src/main/java/ch/giuntini/goodstrainsignalling/service/SignalBoxService.java
+++ src/main/java/ch/giuntini/goodstrainsignalling/service/SignalBoxService.java
@@ -3,14 +3,16 @@ package ch.giuntini.goodstrainsignalling.service;
 import ch.giuntini.goodstrainsignalling.data.DataHandler;
 import ch.giuntini.goodstrainsignalling.model.SignalBox;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * signal box service
