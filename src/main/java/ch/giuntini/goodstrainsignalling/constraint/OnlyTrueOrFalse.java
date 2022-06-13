4e1be4d31710df2f33c03ce4a97fb6888e05f65a
Medox36 <giuntinil@bzz.ch>
2022-06-13 22:15:50 +0200

    improved OnlyTrueOrFalse annotation/constraint

--- src/main/java/ch/giuntini/goodstrainsignalling/constraint/OnlyTrueOrFalse.java
+++ src/main/java/ch/giuntini/goodstrainsignalling/constraint/OnlyTrueOrFalse.java
@@ -6,6 +6,8 @@ import java.lang.annotation.*;
 
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(OnlyTrueOrFalse.List.class)
+@Documented
 @Constraint(validatedBy = BooleanValidatorFromString.class)
 public @interface OnlyTrueOrFalse {
     String message() default "Can only be false or true, also accepted FALSE or TRUE";
@@ -13,4 +15,11 @@ public @interface OnlyTrueOrFalse {
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
+    @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List {
+        OnlyTrueOrFalse[] value();
+    }
 }
