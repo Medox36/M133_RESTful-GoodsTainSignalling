bf14ff0718ce350c27501d54632c427308fa3d15
Medox36 <giuntinil@bzz.ch>
2022-06-27 18:09:39 +0200

    added javadoc to UserService.java

--- src/main/java/ch/giuntini/goodstrainsignalling/service/UserService.java
+++ src/main/java/ch/giuntini/goodstrainsignalling/service/UserService.java
@@ -20,6 +20,13 @@ import java.util.Random;
 @Path("user")
 public class UserService {
 
+    /**
+     * service for logging in
+     *
+     * @param username of the user that wants to log in
+     * @param password of the user that wants to log in
+     * @return the position of the two-factor-authentication word in the users words
+     */
     @POST
     @Path("login")
     @Produces(MediaType.TEXT_PLAIN)
@@ -44,11 +51,22 @@ public class UserService {
                     .status(status)
                     .entity(String.valueOf(wordNo + 1))
                     .cookie(userRoleCookie(user.getRole(), 60),
+                            plainUserRoleCookie(user.getRole(), 60),
                             wordCookie(String.valueOf(wordNo), 60))
                     .build();
         }
     }
 
+    /**
+     * service for checking two-factor-authentication
+     *
+     * @param word position of the two-factor-authentication word in the users words
+     * @param role of the user
+     * @param username of the user
+     * @param password of the user
+     * @param clientWord typed in by the client
+     * @return Response
+     */
     @POST
     @Path("2factor")
     @Produces(MediaType.TEXT_PLAIN)
@@ -73,6 +91,7 @@ public class UserService {
                     .status(status)
                     .entity("")
                     .cookie(userRoleCookie("guest", 1),
+                            plainUserRoleCookie("guest", 1),
                             wordCookie("word", 1))
                     .build();
         }
@@ -82,6 +101,7 @@ public class UserService {
                     .status(status)
                     .entity("")
                     .cookie(userRoleCookie(role, 600),
+                            plainUserRoleCookie(role, 600),
                             wordCookie("word", 1),
                             authCookie("true", 600))
                     .build();
@@ -96,6 +116,11 @@ public class UserService {
 
     }
 
+    /**
+     * service for logging out
+     *
+     * @return Response
+     */
     @DELETE
     @Path("logoff")
     @Produces(MediaType.TEXT_PLAIN)
@@ -110,7 +135,13 @@ public class UserService {
                 .build();
     }
 
-
+    /**
+     * creates a encrypted cookie for authorisation
+     *
+     * @param userRole value to store in the cookie
+     * @param maxAge og the cookie
+     * @return a NewCookie
+     */
     private NewCookie userRoleCookie(String userRole, int maxAge) {
         return new NewCookie(
                 "userRole",
@@ -123,6 +154,32 @@ public class UserService {
         );
     }
 
+    /**
+     * creates a cookie for authorisation
+     *
+     * @param userRole value to store in the cookie
+     * @param maxAge og the cookie
+     * @return a NewCookie
+     */
+    private NewCookie plainUserRoleCookie(String userRole, int maxAge) {
+        return new NewCookie(
+                "userRole",
+                userRole,
+                "/",
+                "", //z.B. abc.ch
+                "Fancy-Cookie",
+                maxAge,
+                false
+        );
+    }
+
+    /**
+     * creates a encrypted cookie for authorisation
+     *
+     * @param value to store in the cookie
+     * @param maxAge og the cookie
+     * @return a NewCookie
+     */
     private NewCookie authCookie(String value, int maxAge) {
         return new NewCookie(
                 "auth",
@@ -135,6 +192,13 @@ public class UserService {
         );
     }
 
+    /**
+     * creates a encrypted cookie for two-factor-authentication
+     *
+     * @param word value to store in the cookie
+     * @param maxAge og the cookie
+     * @return a NewCookie
+     */
     private NewCookie wordCookie(String word, int maxAge) {
         return new NewCookie(
                 "twoFacWord",
@@ -147,6 +211,13 @@ public class UserService {
         );
     }
 
+    /**
+     * checks if a logged in user is an authorized
+     *
+     * @param role of the user
+     * @param auth of the user
+     * @return true if the user is an authorized
+     */
     public static boolean isUserAuthorized(String role, String auth) {
         role = AESEncrypt.decrypt(role);
         auth = AESEncrypt.decrypt(auth);
@@ -156,6 +227,13 @@ public class UserService {
         return (role.equals("admin") || role.equals("user")) && (auth.equals("true"));
     }
 
+    /**
+     * checks if a logged in user is an authorized admin
+     *
+     * @param role of the user
+     * @param auth of the user
+     * @return true if the user is an authorized admin
+     */
     public static boolean isUserAuthorizedAdmin(String role, String auth) {
         role = AESEncrypt.decrypt(role);
         auth = AESEncrypt.decrypt(auth);
